bd59180cac95ff4cf483f7add356f78b3cd77098
Erland Ruiz <erlandruiz@hotmail.com>
2024-12-31 18:23:39 -0500

    secoond commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+import "./App.css";
+import { NavBar, Item } from "./components";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const numero = 5;
+
+  const styleButton = {
+    marginTop: "25px",
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavBar />
+        <div className="container">
+ 
+        <Item/>
+          <div>
+            <a href="https://vite.dev" target="_blank">
+    
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+
+          <span
+            style={{
+              display: "block",
+              fontSize: "10rem",
+            }}
+          >
+            {numero + 1}
+          </span>
+          <h1>Erlandberger</h1>
+          <div className="card">
+            <button
+              style={styleButton}
+              onClick={() => console.log("hola Erland")}
+            >
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
       </div>
-      <h1>Erlandbergrrr</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import { FiShoppingCart } from "react-icons/fi";
+
+export const NavBar = () => {
+  return (
+    <header>
+      <div className="container">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <img
+            src={
+              "https://gandhi.vtexassets.com/assets/vtex.file-manager-graphql/images/378031e7-04bd-4890-bda6-8cef499ff8fd___3d5cf70e5b2ed251829a1c113f688047.png"
+            }
+            className="logo"
+            alt="Vite logo"
+          />
+          <span style={{ fontSize: 18, fontWeight: "bolder" }}>
+            Erland Books
+          </span>
+          <FiShoppingCart style={{fontSize:100, color: "red"}}/>
+          <nav>
+            <a href="">Misterio</a>
+            <a href="">Terror</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
